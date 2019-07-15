61b0b9cb6201f2ce6d7f6f6c44c50a98fa4083ea
Dubeass <1061419123@qq.com>
2019-07-15 23:30:04 +0800

    two

--- src/com/offcn/Demo.java
+++ src/com/offcn/Demo.java
@@ -3,5 +3,10 @@ package com.offcn;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("asdasd");
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(13);
+		System.out.println(123);
 	}
+
 }
